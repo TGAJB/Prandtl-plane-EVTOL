fb32f1de052d168aa0b9a528e7726eb01c3ce13d
TGAJB <a.j.e.brouns@gmail.com>
2026-05-26 12:34:44 +0200

    misc.

---
+++ aeroacoustics.py
@@ -0,0 +1,20 @@
+from constants import 
+
+
+#N2 inputs are listed below
+#wing load distribution
+#propeller diameter
+#number of propeller blades per engine
+#propeller failure loads
+#propeller material seleciton
+#propeller structure
+#power required in different flight phases
+#cabin noise
+
+
+
+
+#N2 outputs of this file
+
+
+
