1b3c926a6eac614612b592a5778ce531e01746fa
TGAJB <a.j.e.brouns@gmail.com>
2026-05-26 12:22:44 +0200

    Update constants and adding call statenebts

--- constants.py
+++ constants.py
@@ -1,14 +1,14 @@
 #this file is responsible for containing constants pertaining to the B&O portion of the N2
 hcruise = #[m/s]
-vcruise =  #[m/s]
-minrange = 200000 #[m]
-htransition = #[m]
-gust_speed = #[m/s]
-cabin_noise_req = #[dB]
-propulsion_type = #[-]
-contingency_energy_reserve = #[kWh]
-Npax = 4 #passengers
-vehicle_lifetime = #years
-m_payload = 400 #[kg]
-aircraft_person_proximity = #[m]
-aircraft_building_proximity = #[m]
+V_CRUISE = 200 / 3.6 #[m/s]
+RANGE_M = 200000.0 #[m]
+htransition = 0#[m]
+gust_speed = 0 #[m/s]
+cabin_noise_req =0 #[dB]
+propulsion_type =0 #[-]
+contingency_energy_reserve =0 #[kWh]
+N_PAX = 4 #passengers
+vehicle_lifetime =0 #years
+M_PAYLOAD = 400 #[kg]
+aircraft_person_proximity =0 #[m]
+aircraft_building_proximity = 0#[m]
