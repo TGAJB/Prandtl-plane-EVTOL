538ca92444f3d85b8624a7246b72b74b8eced656
TGAJB <a.j.e.brouns@gmail.com>
2026-05-26 12:10:16 +0200

    Update Constants

---
+++ constants.py
@@ -0,0 +1,14 @@
+#this file is responsible for containing constants pertaining to the B&O portion of the N2
+hcruise = #[m/s]
+vcruise =  #[m/s]
+minrange = 200000 #[m]
+htransition = #[m]
+gust_speed = #[m/s]
+cabin_noise_req = #[dB]
+propulsion_type = #[-]
+contingency_energy_reserve = #[kWh]
+Npax = 4 #passengers
+vehicle_lifetime = #years
+m_payload = 400 #[kg]
+aircraft_person_proximity = #[m]
+aircraft_building_proximity = #[m]
