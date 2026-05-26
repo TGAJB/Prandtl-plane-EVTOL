4364edb95f9198de7ab43d68916f5fd5e43219a5
TGAJB <a.j.e.brouns@gmail.com>
2026-05-26 14:44:12 +0200

    necessary outputs

--- structures.py
+++ structures.py
@@ -1,2 +1,19 @@
 from constants import *
-from ppe 
+from ppe import *
+from control import *
+from aerodynamics import *
+from vehicle_dynamics import *
+
+
+
+#propeller failure loads
+#propeller structure
+#propeller material selection
+#critical loading areas
+#feasible wing shape
+#fuselage shape
+#CG envelope
+#MMOI
+#MTOW
+#OEW
+#vehicle lifetime
