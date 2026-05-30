583228e7699a7e441824cd3b77e67eb1c1f00a18
TGAJB <a.j.e.brouns@gmail.com>
2026-05-30 16:34:05 +0200

    ppe instructions added

---
+++ ppe.py
@@ -0,0 +1,10 @@
+#ideally this code should end and write new paramters to the fusion_params file so a cad model can be generated
+#consult N2 for inputs and outputs
+#ensure variables are called parametrically (dont recompute stuff)
+
+## This seciton is for geometry updates to propeller, propeller hub, etc. that is done via formula
+#-----------------------------------------
+
+
+##This section is for other stuff
+#-----------------------------------------
