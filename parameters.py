b673801bef8cc94ed5d30c16ea6912d3f1502dfc
wgrefkowicz <wikgrefkowicz@gmail.com>
2026-06-11 09:41:22 +0200

    aero param

--- parameters.py
+++ parameters.py
@@ -61,7 +61,6 @@ TIP_TO_CHORD_W      = 0.17    # [-]     wing thickness-to-chord ratio
 LE_SWEEP_W          = 0       # [rad]   wing leading edge sweep angle
 DIHEDRAL            = 0       # [rad]   wing digedral angle
 TWIST               = 0.05236 # [rad]   wing twist angle (3 deg) NOT FINAL
-h                   = 2.1     # [m]     vertical gap height
 #    Class I parameters (OUTDATED - CLASS II AVAILABLE)
 S_W         = 30.0            # [m^2]   class I total wing reference area 
 AR_W        = 5.63            # [-]     class I wing aspect ratio 
